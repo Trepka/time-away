bea7b5039a04c983bfe065db190d023521cbf49d
Shatskikh Maxim <shatskihms@gmail.com>
2024-02-23 21:17:57 +0300

    initialize

---
+++ src/main/java/com/timeaway/TimeAwayApplication.java
@@ -0,0 +1,13 @@
+package com.timeaway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimeAwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimeAwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/timeaway/TimeAwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.timeaway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimeAwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
